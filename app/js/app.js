681ed2934386053b6bd489fe821871ee963dc2b1
zasipin <zasipin.it@gmail.com>
2015-01-28 19:05:48 +0300

    local storage

--- app/js/app.js
+++ app/js/app.js
@@ -7,8 +7,12 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'LocalStorageModule'
 ]).
+config(['localStorageServiceProvider', function(localStorageServiceProvider){
+	localStorageServiceProvider.prefix ='cr';
+}]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'mainCtrl'});
   //$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});

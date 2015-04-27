96d0ef7b6dc5d9564a91e5b946a814e88db7b8e7
zasipin <zasipin.it@gmail.com>
2015-04-27 21:32:24 +0300

    добавил яндекс метрику

--- app/js/yaMetrics.js
+++ app/js/yaMetrics.js
@@ -19,5 +19,3 @@
         d.addEventListener("DOMContentLoaded", f, false);
     } else { f(); }
 })(document, window, "yandex_metrika_callbacks");
-
-<noscript><div><img src="//mc.yandex.ru/watch/29684095" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
